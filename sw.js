2f61a082bb0d05b0384bdd8e60718fb001e40d22
Dina Rocio <dinarociovq15@hotmail.com>
2021-04-30 13:19:10 -0500

    A clear cache function was created in order to delete old cache versions, after this one is removed, this will immediately activate our service worker when it changes

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
 "use strict";
-//Service workers have introduced a new API named cache API, or cache storage , in order to precached our app shell (check into applicatio in dev tools)
 
-const carDeasCacheName = "carDealsCacheV1"
-
+const carDealsCacheName = "carDealsCacheV2";
+const carDealsCachePagesName = "carDealsCachePagesV1";
+const carDealsCache = [
+  carDealsCacheName,
+  carDealsCachePagesName,
+];
 const carDealsCacheFiles = [
   "https://cdn.jsdelivr.net/npm/pwacompat@2.0.17/pwacompat.min.js",
   "https://cdn.jsdelivr.net/gh/bstavroulakis/progressive-web-apps/resources/localforage.js",
@@ -17,16 +20,25 @@ const carDealsCacheFiles = [
   "index.html",
   "style.css",
 ];
-
 self.addEventListener("install", (event) => {
   console.log("From SW: Instalar Evento");
   const preCache = async () => {
-    const cache = await caches.open(carDeasCacheName);
-    return cache.addAll(carDealsCacheFiles)
+    const cache = await caches.open(carDealsCacheName);
+    return cache.addAll(carDealsCacheFiles);
   };
   //to tell the browser that the service worker won't be considered installed until the promise is resolved or rejected
-  event.waitUntil(preCache()); 
+  event.waitUntil(preCache());
 });
 self.addEventListener("activate", (event) => {
   console.log("From SW: Activar evento");
+  const clearCache = async () => {
+    const keys = await caches.keys();
+    keys.forEach(async (k) => {
+      if (carDealsCache.includes(k)) {
+        return;
+      }
+      await caches.delete(k);
+    });
+  };
+  event.waitUntil(clearCache());
 });
