e5d1791c5bc64b27b31f22dcdf1c8ef9fecd718c
Dina Rocio <dinarociovq15@hotmail.com>
2021-04-30 13:01:31 -0500

    our app shell is been successfully pre cached

--- sw.js
+++ sw.js
@@ -1,7 +1,31 @@
 "use strict";
+//Service workers have introduced a new API named cache API, or cache storage , in order to precached our app shell (check into applicatio in dev tools)
+
+const carDeasCacheName = "carDealsCacheV1"
+
+const carDealsCacheFiles = [
+  "https://cdn.jsdelivr.net/npm/pwacompat@2.0.17/pwacompat.min.js",
+  "https://cdn.jsdelivr.net/gh/bstavroulakis/progressive-web-apps/resources/localforage.js",
+  "js/app.js",
+  "js/carPageService.js",
+  "js/carService.js",
+  "js/clientStorage.js",
+  "js/constants.js",
+  "js/swRegister.js",
+  "js/template.js",
+  "/",
+  "index.html",
+  "style.css",
+];
 
 self.addEventListener("install", (event) => {
   console.log("From SW: Instalar Evento");
+  const preCache = async () => {
+    const cache = await caches.open(carDeasCacheName);
+    return cache.addAll(carDealsCacheFiles)
+  };
+  //to tell the browser that the service worker won't be considered installed until the promise is resolved or rejected
+  event.waitUntil(preCache()); 
 });
 self.addEventListener("activate", (event) => {
   console.log("From SW: Activar evento");
